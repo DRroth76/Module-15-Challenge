6b771427461481e1b8db726ea582a47874cfae5f
DRroth76 <david.randall.roth97@gmail.com>
2024-02-21 12:25:31 -0600

    Made more changes.

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,13 +1,13 @@
 let myMap = L.map("map", {
-    center: [0, 0],
-    zoom: 3
+    center: [0, 5],
+    zoom: 2.5
 });
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
-fetch('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson').then(responser => responser.json()).then(data => {
+fetch('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson').then(responser => responser.json()).then(data => {
     const earthquakes = data;
 
     earthquakes.features.forEach(feature => {
@@ -17,14 +17,18 @@ fetch('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_mon
         const longitude = coordinates[0];
         const magnitude = feature.properties.mag;
         const place = feature.properties.place;
+        const depth = coordinates[2];
+
+        const darkness = 1 - (depth / 100);
 
         const marker = L.circleMarker([latitude, longitude], {
-            radius: magnitude * 5,
-            fillColor: coordinates[2],
+            radius: magnitude * 7,
+            fillColor: `rgb(255, ${Math.floor(255 * (1 - (depth / 100)))}, 0)`,
+            color: 'black',
             weight: 1,
             opacity: 1,
             fillOpacity: 1
-        }).bindPopup(`<b>${place}</b><br />Magnitude: ${magnitude}<br />Latitude: ${latitude}<br />Longitude ${longitude}`);
+        }).bindPopup(`<b>${place}</b><br />Magnitude: ${magnitude}<br />Depth: ${depth}<br />Latitude: ${latitude}<br />Longitude ${longitude}`);
 
         marker.addTo(myMap);
     });
